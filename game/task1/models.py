f8e9398581b1a2b42e9f310ce68ea7b6fe261442
Kirill Tarasov <kir_tarasov@mail.ru>
2024-10-03 18:30:05 +0300

    feat: Complete first task

---
+++ game/task1/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Player(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    points = models.IntegerField(default=0)
+    first_login = models.DateTimeField(auto_now_add=True)
+    bonuses = models.ManyToManyField(
+        "Boost", blank=True, related_name="bonuses"
+    )
+
+
+class BoostType(models.Model):
+    id = models.BigAutoField("id", primary_key=True)
+    name = models.CharField(max_length=255)
+
+
+class Boost(models.Model):
+    id = models.BigAutoField("id", primary_key=True)
+    effect = models.ForeignKey(
+        "BoostType", on_delete=models.PROTECT
+    )
+    duration = models.IntegerField()
+    power = models.IntegerField()
+
