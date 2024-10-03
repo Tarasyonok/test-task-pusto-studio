d258295a44360109aac990989180689f20de9a0b
Kirill Tarasov <kir_tarasov@mail.ru>
2024-10-03 20:51:08 +0300

    feat: Export data to scv file

--- game/task2/functions.py
+++ game/task2/functions.py
@@ -10,3 +10,15 @@ def give_prize(player_id, level_order):
         level_prize.save()
 
     print(f'Игрок {player_id} получил приз "{level_prize.prize.title}"')
+
+
+import csv
+
+
+def export_data_csv():
+    with open("data.csv", mode="w", encoding='utf-8') as w_file:
+        writer = csv.writer(w_file, delimiter=";", lineterminator="\n")
+        data = PlayerLevel.objects.all().values_list('player__player_id', 'level__title', 'is_completed', 'level_id')
+        for row in data:
+            level_prize = LevelPrize.objects.filter(level_id=row[-1]).first()
+            writer.writerow(row[:-1] + (level_prize.prize.title,))
