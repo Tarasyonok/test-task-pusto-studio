bf599f23447a11a9fb655be8189c67a6723758d8
Kirill Tarasov <kir_tarasov@mail.ru>
2024-10-04 10:10:27 +0300

    feat: Print first five rows of date

--- game/task2/functions.py
+++ game/task2/functions.py
@@ -17,6 +17,9 @@ def export_data_csv():
     with open("data.csv", mode="w", encoding='utf-8') as w_file:
         writer = csv.writer(w_file, delimiter=";", lineterminator="\n")
         data = PlayerLevel.objects.all().values_list('player__player_id', 'level__title', 'is_completed', 'level_id')
-        for row in data:
+        for i, row in enumerate(data):
             level_prize = LevelPrize.objects.filter(level_id=row[-1]).first()
+            # Первые пять значений, которые будут записаны в файл
+            if i < 5:
+                print(row[:-1] + (level_prize.prize.title,))
             writer.writerow(row[:-1] + (level_prize.prize.title,))
